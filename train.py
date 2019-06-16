87ef33fa2d50633f59b1e99c3ef5c2cf8d2a0273
Mariam <maryammohamed61@gmail.com>
2019-06-16 12:00:24 +0200

    fix3

--- train.py
+++ train.py
@@ -11,7 +11,7 @@ from include.model import model, lr
 
 train_x, train_y = get_data_set("train")
 test_x, test_y = get_data_set("test")
-tf.random.set_seed(21)
+tf.set_random_seed(21)
 x, y, output, y_pred_cls, global_step, learning_rate = model()
 global_accuracy = 0
 epoch_start = 0
